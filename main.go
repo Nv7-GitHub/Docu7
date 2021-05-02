afa60a9c2a9e93d69e366e1108c993819cd10dcc
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2021-05-02 16:35:48 -0700

    Circles and some other stuff

--- main.go
+++ main.go
@@ -1,15 +1,48 @@
 package main
 
+// The idea is to use bild's shear X & Y + having the user mark the 4 points to make an epic doc scanner
+
 import (
+	"image"
+	"os"
+
+	"image/color"
+	_ "image/jpeg"
+	_ "image/png"
+
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/widget"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
 )
+
+var win fyne.Window
+
+func handle(err error) {
+	if err != nil {
+		dialog.ShowError(err, win)
+		panic(err)
+	}
+}
 
 func main() {
 	app := app.New()
+	win = app.NewWindow("Docu7")
 
-	w := app.NewWindow("Hello")
-	w.SetContent(widget.NewLabel("Hello Fyne!"))
+	// Load file from file dialog instead
+	file, err := os.Open("input.png")
+	handle(err)
+	img, _, err := image.Decode(file)
+	handle(err)
 
-	w.ShowAndRun()
+	image := canvas.NewImageFromImage(img)
+	image.FillMode = canvas.ImageFillContain
+
+	circle := canvas.NewCircle(color.RGBA{R: 255, G: 0, B: 0, A: 255})
+
+	box := container.NewMax(image, circle)
+	win.SetContent(box)
+	win.Resize(fyne.NewSize(850, 500))
+	win.ShowAndRun()
 }
